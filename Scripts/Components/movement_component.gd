fbde542f57eaf07edbbf738763165f48525c25d1
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-04-10 21:44:13 +0800

    added commits and to-dos
    This adds commits to detail code segments, and includes TO DOs for further code improvement.

--- Scripts/Components/movement_component.gd
+++ Scripts/Components/movement_component.gd
@@ -1,7 +1,10 @@
 extends Node2D
 class_name MovementComponent
 
+# Variables
 @export var speed : int
 
+# Function for calculating the movement of the player
+# movement is a Vector2 that shows the direction of the player
 func calculate_movement(movement: Vector2) -> Vector2:
 	return movement * speed 
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+# Player Components
 @export var input_component: InputComponent
 @export var movement_component: MovementComponent
-@export var animation_component: AnimatedSprite2D
+@export var animation_component: AnimatedSprite2D # TO DO: Move this to AnimationComponent
 
+# TO DO: Move this to AnimationComponent
 var last_faced_direction := Vector2.ZERO
 
 func update_animation(movement: Vector2) -> void:
@@ -29,10 +31,13 @@ func update_animation(movement: Vector2) -> void:
 		elif last_faced_direction.y < 0:
 			animation_component.play("idle_up")
 
+# TO DO: Make this function to cater to a particular player
+# Function that connects to when player1 is moved
+# movement is a Vector2 that shows the direction of the player
 func _on_input_component_player_1_moved(movement: Vector2) -> void:
 	var player_movement = movement_component.calculate_movement(movement)
 	
+	# Update movement and animation
 	velocity = player_movement
 	update_animation(player_movement)
-	
 	move_and_slide()
