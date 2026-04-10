e683b00ce61bed75f64d17e89e3303e53884f83b
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-04-10 15:19:47 +0800

    added input component and input mapping
    This adds signals for player 1 and player 2 movement, and input mappings from keyboard.

---
+++ Scripts/input_component.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Signals for movement
+signal player1_moved(movement: Vector2)
+signal player2_moved(movement: Vector2)
+
+func _process(delta: float) -> void:
+	# Get movement vector and emit through each player
+	var p1_movement = Input.get_vector(
+		"player1_left", 
+		"player1_right", 
+		"player1_up", 
+		"player1_down")
+	var p2_movement = Input.get_vector(
+		"player2_left",
+		"player2_right",
+		"player2_up", 
+		"player2_down")
+	
+	player1_moved.emit(p1_movement)
+	player2_moved.emit(p2_movement)
