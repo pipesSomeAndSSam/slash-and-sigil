b535fcfc326f62462448ca2350d72178160a4ea4
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-04-12 22:11:44 +0800

    added health component
    It introduces ```HealthComponent.tscn```, which manages the health of the player.

---
+++ Scripts/Components/health_component.gd
@@ -0,0 +1,21 @@
+# Component for managing player health
+extends Node2D
+class_name HealthComponent
+
+# Variables
+@export var health : int
+
+# Signals
+signal player_damaged(damage: int)
+signal player_died
+
+# Function that calculates the damage taken by the player
+# damage is an int that the player takes
+func take_damage(damage: int) -> void:
+	# Deduct player health with damage and emit signal that the player has damaged
+	health = max(0, health - damage)
+	player_damaged.emit(damage)
+	
+	# Emit signal that the player has died if health has reached 0 
+	if not health:
+		player_died.emit()
