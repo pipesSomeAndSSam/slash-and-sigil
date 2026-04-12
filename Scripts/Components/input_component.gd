d8478995c503a0b467dd748e2df957b3b60c3427
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-04-13 00:02:34 +0800

    added attack and skill binds
    It includes an input mapping for attack and skill binds in the InputComponent.

--- Scripts/Components/input_component.gd
+++ Scripts/Components/input_component.gd
@@ -4,9 +4,13 @@ class_name InputComponent
 
 # Variables
 @export var player_number: int
+@onready var player_number_string := "player%d_" % player_number
 
-# Signals for movement
+# Signals for button presses
 signal player_moved(movement: Vector2)
+signal attack_pressed
+signal skill1_pressed
+signal skill2_pressed
 
 func _physics_process(delta: float) -> void:
 	# Ensures only 2 players are available in the game.
@@ -14,13 +18,21 @@ func _physics_process(delta: float) -> void:
 		return
 		
 	# Get movement vector from player
-	var p = "player%d_" % player_number
 	var p_movement = Input.get_vector(
-		p+"left",
-		p+"right",
-		p+"up",
-		p+"down"
+		player_number_string + "left",
+		player_number_string + "right",
+		player_number_string + "up",
+		player_number_string + "down"
 	)
 	
 	# Emit signal indicating the player has moved
 	player_moved.emit(p_movement)
+
+func _input(event: InputEvent) -> void:
+	# Emit signals for when attack and skill keys are pressed
+	if event.is_action_pressed(player_number_string + "attack"):
+		attack_pressed.emit()
+	elif event.is_action_pressed(player_number_string + "skill1"):
+		skill1_pressed.emit()
+	elif event.is_action_pressed(player_number_string + "skill2"):
+		skill2_pressed.emit()
