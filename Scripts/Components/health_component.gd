1e6b26e924625c560e50ec5ec8c8e9f287eac802
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-04-15 03:55:53 +0800

    changed health datatype
    It changes the health datatype from int to float for more flexibility.

--- Scripts/Components/health_component.gd
+++ Scripts/Components/health_component.gd
@@ -3,15 +3,15 @@ extends Node2D
 class_name HealthComponent
 
 # Variables
-@export var health : int
+@export var health : float
 
 # Signals
-signal player_damaged(damage: int)
+signal player_damaged(damage: float)
 signal player_died
 
 # Function that calculates the damage taken by the player
 # damage is an int that the player takes
-func take_damage(damage: int) -> void:
+func take_damage(damage: float) -> void:
 	# Deduct player health with damage and emit signal that the player has damaged
 	health = max(0, health - damage)
 	player_damaged.emit(damage)
